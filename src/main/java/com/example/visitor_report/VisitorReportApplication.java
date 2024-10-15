cf08e0dd196e84ee7816fb5aaf42f767f1dd6d12
Lucas Okada <leiki@ciandt.com>
2024-10-14 23:38:47 -0300

    inicializa projeto

---
+++ src/main/java/com/example/visitor_report/VisitorReportApplication.java
@@ -0,0 +1,13 @@
+package com.example.visitor_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/visitor_report/VisitorReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.visitor_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
